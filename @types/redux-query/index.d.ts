605637317fa832db157a5fb22195c588d1d66c5e
Jean Regisser <jean.regisser@gmail.com>
2019-05-10 19:14:28 +0200

    Format with prettier

--- @types/redux-query/index.d.ts
+++ @types/redux-query/index.d.ts
@@ -1,10 +1,10 @@
 declare module 'redux-query' {
   var x: {
-    connectRequest: any,
-    querySelectors: any,
-    entitiesReducer: any,
-    queriesReducer: any,
-    queryMiddleware: any,
+    connectRequest: any;
+    querySelectors: any;
+    entitiesReducer: any;
+    queriesReducer: any;
+    queryMiddleware: any;
   };
   export = x;
 }
